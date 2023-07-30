1eed62e8edf7765b3a4894b6f0d88dadceb05841
unknown <binobajichiku@gmail.com>
2023-07-30 21:21:01 +0530

    updated the old chat

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -51,26 +51,50 @@ connectDatabase().then(() => {
 
 io.on("connection",(socket)=>{
 
+  socket.on("show",async({Id1,Id2})=>{
+  
+   let userChat=await Chart.findOne({$or:[{Id1,Id2},{Id1:Id2,Id2:Id1}]});
+    // console.log(userChat);
+    if(userChat===null||Object.keys(userChat).length===0){
+      socket.emit(Id1+Id2,{err:true});
+    }
+    else{
+    let arr=userChat.Chat;
+    
+    let err=false;
+    io.emit(Id1+Id2,{arr,err});
+    io.emit(Id2+Id1,{arr,err});}
+
+  })
+
  socket.on("chat",async({Id1,Id2,Message})=>{
   try {
+    let userChat={};
+    let Chat=[];
     if(Id1&&Id2&&Message){
-      let userChat={};
-  
-      userChat=Chart.find({$or:[{Id1,Id2},{Id1:Id2,Id2:Id1}]});
+     
+      userChat=await Chart.findOne({$or:[{Id1,Id2},{Id1:Id2,Id2:Id1}]});
       if(userChat===null||Object.keys(userChat).length===0){
-        let Chat=[];
-        Chat.push(Message);
-        await Chart.create({Id1,Id2,Message});
+        Chat.push({Id1,Message});
+        await Chart.create({Id1,Id2,Chat});
       }
-  
-      userChat=Chart.find({$or:[{Id1,Id2},{Id1:Id2,Id2:Id1}]});
-      let arr=userChat.Chat;
+      else{
+        Chat=userChat.Chat;
+        Chat.push({Id1,Message});
+        // console.log(userChat.Chat);
+        // userChat.Chat.push({Id1,Message})
+        await Chart.updateOne({_id:userChat._id},{$set:{Chat:[...Chat]}});
+    }
+
       let err=false;
-      socket.emit(Id1+Id2,{arr,err});
-      io.emit(Id2+Id1,{arr,err});
+      
+      socket.emit(Id1+Id2,{Chat,err});
+      io.emit(Id2+Id1,{Chat,err});
     }
   } catch (error) {
-    console.log("error on line no 74")
+    let err=true;
+    console.log("error on line no 74");
+    socket.emit(Id1+Id2,{err});
   } 
  })
 
