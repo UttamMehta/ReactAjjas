4fc6460ed216ee5e42ca96b13e0cc6c44ed3c4f0
unknown <binobajichiku@gmail.com>
2023-07-30 20:36:51 +0530

    added new way

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -51,6 +51,28 @@ connectDatabase().then(() => {
 
 io.on("connection",(socket)=>{
 
+ socket.on("chat",async({Id1,Id2,Message})=>{
+  try {
+    if(Id1&&Id2&&Message){
+      let userChat={};
+  
+      userChat=Chart.find({$or:[{Id1,Id2},{Id1:Id2,Id2:Id1}]});
+      if(userChat===null||Object.keys(userChat).length===0){
+        let Chat=[];
+        Chat.push(Message);
+        await Chart.create({Id1,Id2,Message});
+      }
+  
+      userChat=Chart.find({$or:[{Id1,Id2},{Id1:Id2,Id2:Id1}]});
+      let arr=userChat.Chat;
+      let err=false;
+      socket.emit(Id1+Id2,{arr,err});
+      io.emit(Id2+Id1,{arr,err});
+    }
+  } catch (error) {
+    console.log("error on line no 74")
+  } 
+ })
 
 socket.on("disconnect",()=>{
 console.log("disconnected")
